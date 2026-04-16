08ea9703f8af6e80f542bd1c934477667943a19c
Jade <jnaylor344@gmail.com>
2026-04-16 19:44:31 +0100

    Update SQL analysis queries

--- queries.sql
+++ queries.sql
@@ -1,1 +1,8 @@
 SELECT * FROM transactions;
+SELECT COUNT(*) AS total_transactions FROM transactions;
+SELECT type, SUM(amount) 
+FROM transactions 
+GROUP BY type;
+SELECT user_id, SUM(amount) AS total_spent
+FROM transactions
+GROUP BY user_id;
